96bcaaf16b484be3f3d0665eb9783d95fc17bffd
HarshaVardhanRao <Hvijapuram@gmail.com>
2025-07-11 11:59:48 +0530

    Done shortening Urls

--- Backend Test Submission/backend/MainApp.py
+++ Backend Test Submission/backend/MainApp.py
@@ -59,7 +59,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/shorten", response_model=ShortenResponse)
+@app.post("/shorturls", response_model=ShortenResponse)
 async def make_short_url(user_data: ShortenRequest, db: Session = Depends(get_db)):
     short_code = user_data.custom_shortcode or make_random_code()
 
