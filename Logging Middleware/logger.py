7aa46d054b1c185e3cdb186f1da55282a9b97bb9
HarshaVardhanRao <Hvijapuram@gmail.com>
2025-07-11 11:30:44 +0530

    LogID received and added in File

--- Logging Middleware/logger.py
+++ Logging Middleware/logger.py
@@ -1,11 +1,15 @@
 import httpx
 
-BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJodmlqYXB1cmFtQGdtYWlsLmNvbSIsImV4cCI6MTc1MjIxMzIyOSwiaWF0IjoxNzUyMjEyMzI5LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZGFjOWQ3ZjAtMDNkMi00YzNkLWE5NzMtZWRjZmVmOGUzYzg0IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidmlqYXB1cmFtIGhhcnNoYXZhcmRoYW4iLCJzdWIiOiJlZWJjMGM0Zi0zNjdhLTRhN2QtOTY0NC0xYjNhNGE4ZWQ1NTAifSwiZW1haWwiOiJodmlqYXB1cmFtQGdtYWlsLmNvbSIsIm5hbWUiOiJ2aWphcHVyYW0gaGFyc2hhdmFyZGhhbiIsInJvbGxObyI6IjIyNjkxYTA1NzEiLCJhY2Nlc3NDb2RlIjoiY2FWdk5IIiwiY2xpZW50SUQiOiJlZWJjMGM0Zi0zNjdhLTRhN2QtOTY0NC0xYjNhNGE4ZWQ1NTAiLCJjbGllbnRTZWNyZXQiOiJiaEpZd3JVQlpnUWJBcXR6In0.U9wWCRweQPWwOgR8SyYiCOhewFKvd3vIwiV3d5V7qko"
+BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJodmlqYXB1cmFtQGdtYWlsLmNvbSIsImV4cCI6MTc1MjIxNDQwMSwiaWF0IjoxNzUyMjEzNTAxLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiNzQ5NzA4OTktMGYzOC00ODIwLWI4OGYtNjZmNGNmZDFmMmQxIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidmlqYXB1cmFtIGhhcnNoYXZhcmRoYW4iLCJzdWIiOiJlZWJjMGM0Zi0zNjdhLTRhN2QtOTY0NC0xYjNhNGE4ZWQ1NTAifSwiZW1haWwiOiJodmlqYXB1cmFtQGdtYWlsLmNvbSIsIm5hbWUiOiJ2aWphcHVyYW0gaGFyc2hhdmFyZGhhbiIsInJvbGxObyI6IjIyNjkxYTA1NzEiLCJhY2Nlc3NDb2RlIjoiY2FWdk5IIiwiY2xpZW50SUQiOiJlZWJjMGM0Zi0zNjdhLTRhN2QtOTY0NC0xYjNhNGE4ZWQ1NTAiLCJjbGllbnRTZWNyZXQiOiJiaEpZd3JVQlpnUWJBcXR6In0.BTDsWJ8Au_8i_0pc-LtJWZOGCCm4ALjuMpg9LuNqE2M"
 
 LOG_URL = "http://20.244.56.144/evaluation-service/logs"
 
-async def Logme(stack: str, level: str, package: str, message: str):
-    headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
+async def Log(stack: str, level: str, package: str, message: str):
+    headers = {
+        "Authorization": f"Bearer {BEARER_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
     payload = {
         "stack": stack,
         "level": level,
@@ -17,8 +21,18 @@ async def Logme(stack: str, level: str, package: str, message: str):
         async with httpx.AsyncClient() as client:
             response = await client.post(LOG_URL, json=payload, headers=headers)
             response.raise_for_status()
-            print(f"Log sent: {payload}")
+            data = response.json()
+            print(f"Log sent: {data}")
+            return data
     except httpx.RequestError as e:
         print(f"Connection error: {e}")
     except httpx.HTTPStatusError as e:
         print(f"HTTP error: {e.response.text}")
+
+
+
+""" 
+Log sent: {'logID': 'fe56ac59-cbec-4044-bb5a-8d8f2372c19f', 'message': 'log created successfully'}
+Log sent: {'logID': '5f6ab72e-28f7-4808-899b-e0ecb2163a07', 'message': 'log created successfully'}
+Log sent: {'logID': 'db59b88b-43f2-485a-9337-e1952e19391d', 'message': 'log created successfully'}
+"""
--- Logging Middleware/main.py
+++ Logging Middleware/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from logger import Logme
+from logger import Log
 import asyncio
 
 app = FastAPI()
@@ -7,16 +7,16 @@ app = FastAPI()
 @app.middleware("http")
 async def logging_middleware(request: Request, call_next):
 
-    await Logme("backend", "info", "middleware", f"Incoming request: {request.method} {request.url}")
+    await Log("backend", "info", "middleware", f"Incoming request: {request.method} {request.url}")
     
     response = await call_next(request)
 
 
-    await Logme("backend", "info", "middleware", f"Response status: {response.status_code}")
+    await Log("backend", "info", "middleware", f"Response status: {response.status_code}")
     
     return response
 
 @app.get("/test")
 async def test():
-    await Logme("backend", "error", "handler", "Something went wrong in /test")
+    await Log("backend", "error", "handler", "Something went wrong in /test")
     return {"message": "Testing log system"}
